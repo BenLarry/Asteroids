85800a86ee24ab7f5d7624b5b64d28baff1d85e3
BenLarry <4muki4@gmail.com>
2024-10-23 23:52:29 +0300

    shooting cooldown

--- main.py
+++ main.py
@@ -10,36 +10,46 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    bullets = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
-    Shot.containers(bullets, updatable, drawable)
     asteroid_field = AsteroidField()
 
-    dt = 0
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(0)
+
         for obj in updatable:
             obj.update(dt)
 
         for asteroid in asteroids:
             if asteroid.check_collisions(player):
-                print("Game Over!")
+                print("Game over!")
                 quit()
+
+        screen.fill("black")
+
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
